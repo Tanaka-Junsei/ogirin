d0db29a08d178f93ce35d870c15bc92796aa7eb2
Tanaka-Junsei <ca02447@cartahd.com>
2024-12-31 02:18:01 +0900

    一旦コンポーネント作った

---
+++ client/app/components/textBox.tsx
@@ -0,0 +1,9 @@
+const TextBox = () => {
+    return (
+      <p className="text-center text-3xl">
+        テキストを表示
+      </p>
+    );
+  };
+  
+  export default TextBox;
---
+++ client/app/components/whiteButton.tsx
@@ -0,0 +1,9 @@
+const WhiteButton = () => {
+    return (
+      <button className="bg-white px-6 py-3 rounded-md text-xl cursor-pointer">
+        ボタン
+      </button>
+    );
+  };
+  
+  export default WhiteButton;
