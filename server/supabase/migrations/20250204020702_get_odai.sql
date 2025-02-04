310b1e332919191b6f7210395d2448f064444963
StupidYoshiaki <nyanyap0418@gmail.com>
2025-02-04 11:23:14 +0900

    これも変更よ

---
+++ server/supabase/migrations/20250204020702_get_odai.sql
@@ -0,0 +1,16 @@
+create or replace function get_odai()
+returns table(id uuid, text text, created_at timestamp, view_count int)
+language sql
+as $$
+  with selected as (
+    select * from odai
+    order by view_count asc, created_at asc
+    limit 1
+  ), updated as (
+    update odai
+    set view_count = view_count + 1
+    where id = (select id from selected)
+    returning *
+  )
+  select * from updated;
+$$;
